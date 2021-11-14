5cd50daeb94aea848364df9c60d71ff81a730e94
trevorius <trevorius45@gmail.com>
2021-11-14 07:42:38 -0500

    moved tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,18 +1,19 @@
-require('dotenv').config()
+require('dotenv').config();
 
 // import test function from app file to a constant
-const test = require('./app');
+const test = require('../app');
 // first test
 describe('test', () => {
   it('should return a string', () => {
     // assert that the test function returns a string
-    expect(typeof test()).toBe("string")
+    expect(typeof test()).toBe("string");
   });
 });
 // second test
 describe('test', () => {
   it("should return value from .env file ", () => {
     // assert that the test function returns the value from .env file
-    expect(test()).toBe(process.env.TEST_VAR)
+    expect(test()).toBe(process.env.TEST_VAR);
   });
 });
+
