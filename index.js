0e39d23ab7032504cc649f8d2fad5c21115d25c4
trevorius <trevorius45@gmail.com>
2021-11-14 08:00:46 -0500

    index.test

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-//listen on 3000
-app.listen(3000, () => {
-  console.log('listening on 3000');
-});
+// //listen on 3000
+// app.listen(3000, () => {
+//   console.log('listening on 3000');
+
+//   // get request returns list of names as json
+//   app.get('/', (req, res) => {
+//     res.json([
+//       { name: 'bob' },
+//       { name: 'joe' },
+//       { name: 'sue' }
+//     ]);
+
+
+//   });
+// });
 
