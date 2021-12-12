aaaf9e67fd600a21d7a7bba6ed54be99e2b042f1
trevorius <trevorius45@gmail.com>
2021-12-12 11:20:20 -0500

    test connection on run

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 require('dotenv').config();
-
+const ENV = process.env.NODE_ENV || 'development';
 
 // import server and listen
 const app = require('./app/server');
+const db = require('./config/db/db');
+
+testConnection = async () => {
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+testConnection();
+
+console.log(`ENV: ${ENV}`);
+
+
 const port = process.env.PORT || 3000;
 app.listen(port);
+console.log(`Server listening on port ${port}`);
