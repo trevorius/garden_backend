ab892a6930a7ec84ba17623204dd47141a9983e7
trevorius <trevorius45@gmail.com>
2021-12-19 11:07:30 -0500

    ✅ Unhappy path

--- tests/server.test.js
+++ tests/server.test.js
@@ -13,9 +13,19 @@ it('gets the test endpoint', async () => {
 
   expect(response.body.message).toBe('pass!');
 });
+describe('Watered', () => {
 
-it(' posts to beds/watered with valid body', async () => {
-  const response = await request.post('/beds/watered').send({ id: 1 });
+  it(' posts to beds/watered with valid body', async () => {
+    const response = await request.post('/beds/watered').send({ id: 1 });
+    expect(response.status).toBe(201);
+  });
 
-  expect(response.status).toBe(201);
+  it('returns error if submitted Bed Id is invalid', async () => {
+    const response = await request.post('/beds/watered').send({ id: 'a' });
+    expect(response.status).toBe(400);
+  });
+  it('returns NotFound if submitted Id is not in db', async () => {
+    const response = await request.post('/beds/watered').send({ id: 100 });
+    expect(response.status).toBe(404);
+  });
 });
